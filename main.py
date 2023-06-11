cf688af5d0ba406694e50129bcd9f562c44f47e3
abi23 <ragunath.abinaya@gmail.com>
2023-06-11 10:52:29 -0700

    monday_motivation code

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
+import smtplib
 import random
-import sys
-import math
+import datetime as dt
+import json
 
-x = random.randint(0,9)
-print(x)
+# Send email
+def send_email():
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=login_data["from_email"], password=login_data["from_password"])
+        connection.sendmail(from_addr=login_data["from_email"], to_addrs=login_data["to_email"], msg=f"Subject:Weekday Motivation\n\n{random_quote}")
+
+# Checking for email/password file:
+try:
+    with open ("secret.json", mode='r') as data:
+        login_data = json.load(data)
+except:
+    print("secret.json with login details is missing.")
+else:
+    # Creating a list of quotes
+    with open ("quotes.txt", mode='r') as data:
+        quotes_data = data.readlines()
+        random_quote = random.choice(quotes_data)
+
+    # Find today's day
+    now = dt.datetime.now()
+    day_of_the_week = now.strftime("%A")
+
+    if day_of_the_week == 'Monday':
+        send_email()
+
+
+
+
